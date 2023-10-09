1bde88a0930ed164575534e98fe9bea722fb288d
Shubham Mehta <12875355+ZeusEm@users.noreply.github.com>
2023-10-09 21:25:02 +0530

    Update analytics.py 091023

--- analytics.py
+++ analytics.py
@@ -88,11 +88,15 @@ Based on the output, it appears that there are multiple issues with the data in 
 The code below addresses these issues by skipping rows, handling missing values, and ensuring proper data types
 """
 
-# Load the 'Local Currency' worksheet and skip the first few rows (skiprows)
+# Load the worksheets and skip the first few rows (skiprows)
 local_currency_df = pd.read_excel(file_path, sheet_name='Local Currency', skiprows=4)
+current_usd_df = pd.read_excel(file_path, sheet_name='Current USD', skiprows=8)
+constant_usd_df = pd.read_excel(file_path, sheet_name='Constant USD', skiprows=8)
 
 # Replace '. .' and other placeholders for missing values with NaN
 local_currency_df = local_currency_df.replace('. .', pd.NA)
+current_usd_df = current_usd_df.replace('. .', pd.NA)
+constant_usd_df = constant_usd_df.replace('. .', pd.NA)
 
 # Handle missing values based on your analysis needs (e.g., imputation or removal)
 # Consider why data is missing. If it's missing completely at random (MCAR), meaning the probability of data being missing is unrelated to the values themselves, dropping rows may be a reasonable option. However, if the missing data is not MCAR (e.g., missing systematically or based on certain conditions), dropping rows might lead to biased results.
@@ -103,20 +107,48 @@ local_currency_df = local_currency_df.replace('. .', pd.NA)
 # For example, if you have numerical columns, you can convert them to float or int:
 # local_currency_df['Column_Name'] = local_currency_df['Column_Name'].astype(float)
 
-# Check the structure of the cleaned DataFrame
+# Check the structure of the cleaned DataFrames
 print("Cleaned Local Currency DataFrame:")
 print(local_currency_df.head())
 
-# Check for missing values and data types in the cleaned DataFrame
+print("Cleaned Current USD DataFrame:")
+print(current_usd_df.head())
+
+print("Cleaned Constant USD DataFrame:")
+print(constant_usd_df.head())
+
+# Check for missing values and data types in the cleaned Local Currency DataFrame
 print("\nMissing Values in Cleaned Local Currency DataFrame:")
 print(local_currency_df.isnull().sum())
 
 print("\nData Types and Info for Cleaned Local Currency DataFrame:")
 print(local_currency_df.info())
 
-# Check basic statistics for the cleaned DataFrame
+# Check basic statistics for the cleaned Local Currency DataFrame
 print("\nBasic Statistics for Cleaned Local Currency DataFrame:")
 print(local_currency_df.describe())
+
+# Check for missing values and data types in the cleaned Current USD DataFrame
+print("\nMissing Values in Cleaned Current USD DataFrame:")
+print(current_usd_df.isnull().sum())
+
+print("\nData Types and Info for Cleaned Current USD DataFrame:")
+print(current_usd_df.info())
+
+# Check basic statistics for the cleaned Current USD DataFrame
+print("\nBasic Statistics for Cleaned Current USD DataFrame:")
+print(current_usd_df.describe())
+
+# Check for missing values and data types in the cleaned Constant USD DataFrame
+print("\nMissing Values in Cleaned Constant USD DataFrame:")
+print(constant_usd_df.isnull().sum())
+
+print("\nData Types and Info for Cleaned Constant USD DataFrame:")
+print(constant_usd_df.info())
+
+# Check basic statistics for the cleaned Constant USD DataFrame
+print("\nBasic Statistics for Cleaned Constant USD DataFrame:")
+print(constant_usd_df.describe())
 
 # Rename columns
 new_column_names = ['Country', 'Currency'] + [f'Year_{year}' for year in range(1994, 2020)] + ['Explanation of data', 'Comments', 'Sources of data']
@@ -126,8 +158,69 @@ local_currency_df.columns = new_column_names
 numeric_columns = new_column_names[2:-3]  # Columns from Year_1994 to Year_2019
 local_currency_df[numeric_columns] = local_currency_df[numeric_columns].apply(pd.to_numeric, errors='coerce')
 
+# Rename columns
+new_column_names = ['Country'] + [f'Year_{year}' for year in range(2001, 2020)] + ['Explanation of data', 'Comments', 'Sources of data']
+current_usd_df.columns = new_column_names
+
+# Convert numeric columns to appropriate data types
+numeric_columns = new_column_names[1:-3]  # Columns from Year_2001 to Year_2019
+current_usd_df[numeric_columns] = current_usd_df[numeric_columns].apply(pd.to_numeric, errors='coerce')
+
+# Rename columns
+new_column_names = ['Country'] + [f'Year_{year}' for year in range(2001, 2020)] + ['Explanation of data', 'Comments', 'Sources of data']
+constant_usd_df.columns = new_column_names
+
+# Convert numeric columns to appropriate data types
+numeric_columns = new_column_names[1:-3]  # Columns from Year_2001 to Year_2019
+constant_usd_df[numeric_columns] = constant_usd_df[numeric_columns].apply(pd.to_numeric, errors='coerce')
+
 # Check the structure and data types after cleaning
 print("Cleaned and Renamed Local Currency DataFrame:")
 print(local_currency_df.head())
 print("\nData Types and Info for Cleaned Local Currency DataFrame:")
 print(local_currency_df.info())
+
+# Check the structure and data types after cleaning
+print("Cleaned and Renamed Current USD DataFrame:")
+print(current_usd_df.head())
+print("\nData Types and Info for Cleaned Current USD DataFrame:")
+print(current_usd_df.info())
+
+# Check the structure and data types after cleaning
+print("Cleaned and Renamed Constant USD DataFrame:")
+print(current_usd_df.head())
+print("\nData Types and Info for Cleaned Constant USD DataFrame:")
+print(current_usd_df.info())
+
+# Exploratory Data Analysis (EDA) commences
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Choose a country for analysis (e.g., Albania)
+country = 'India'
+
+# Filter the data for the selected country
+country_data_local = local_currency_df[local_currency_df['Country'] == country]
+country_data_current_usd = local_currency_df[local_currency_df['Country'] == country]
+country_data_constant_usd = local_currency_df[local_currency_df['Country'] == country]
+
+# Extract years and military expenditure values for the selected country
+years = range(1994, 2020)
+military_expenditure_local = np.nan_to_num(country_data_local.iloc[:, 2:29].values.flatten(), nan=0)
+military_expenditure_current_usd = np.nan_to_num(country_data_current_usd.values.flatten()[1:], nan=0)
+military_expenditure_constant_usd = np.nan_to_num(country_data_constant_usd.values.flatten()[1:], nan=0)
+
+# Create a time series plot for military expenditure
+plt.figure(figsize=(12, 6))
+plt.plot(years, military_expenditure_local, label='Local Currency', marker='o')
+plt.plot(years, military_expenditure_current_usd, label='Current USD', marker='o')
+plt.plot(years, military_expenditure_constant_usd, label='Constant USD', marker='o')
+plt.title(f'Military Expenditure Over Time for {country}')
+plt.xlabel('Year')
+plt.ylabel('Military Expenditure (Million USD)')
+plt.xticks(years, rotation=45)
+plt.grid(True)
+plt.legend()
+plt.tight_layout()
+plt.show()
