0677b477c3c4fb4dd4774efb7d6d4bc3d24c95d4
Shubham Mehta <12875355+ZeusEm@users.noreply.github.com>
2023-10-10 21:13:45 +0530

    Update analytics.py 101023

--- analytics.py
+++ analytics.py
@@ -68,16 +68,6 @@ print(current_usd_df.info())
 print("\nData Types and Info for Constant USD DataFrame:")
 print(constant_usd_df.info())
 
-# Check basic statistics for each DataFrame
-print("\nBasic Statistics for Local Currency DataFrame:")
-print(local_currency_df.describe())
-
-print("\nBasic Statistics for Current USD DataFrame:")
-print(current_usd_df.describe())
-
-print("\nBasic Statistics for Constant USD DataFrame:")
-print(constant_usd_df.describe())
-
 """
 Based on the output, it appears that there are multiple issues with the data in the 'Local Currency' worksheet:
 
@@ -89,7 +79,7 @@ The code below addresses these issues by skipping rows, handling missing values,
 """
 
 # Load the worksheets and skip the first few rows (skiprows)
-local_currency_df = pd.read_excel(file_path, sheet_name='Local Currency', skiprows=4)
+local_currency_df = pd.read_excel(file_path, sheet_name='Local Currency', skiprows=8)
 current_usd_df = pd.read_excel(file_path, sheet_name='Current USD', skiprows=8)
 constant_usd_df = pd.read_excel(file_path, sheet_name='Constant USD', skiprows=8)
 
@@ -97,6 +87,61 @@ constant_usd_df = pd.read_excel(file_path, sheet_name='Constant USD', skiprows=8
 local_currency_df = local_currency_df.replace('. .', pd.NA)
 current_usd_df = current_usd_df.replace('. .', pd.NA)
 constant_usd_df = constant_usd_df.replace('. .', pd.NA)
+
+def split_and_clean(df):
+    # Initialize empty DataFrames for exports and licences
+    df_exports = pd.DataFrame(columns=df.columns)
+    df_licences = pd.DataFrame(columns=df.columns)
+
+    # Initialize variables to store country name for licences
+    current_country = None
+    licences_to_append = []
+
+    for index, row in df.iterrows():
+        explanation = row['Explanation of data']
+
+        # Check if explanation is not NaN
+        if not pd.isna(explanation) and 'arms exports' in explanation.lower():
+            # This row belongs to exports
+            df_exports = pd.concat([df_exports, pd.DataFrame(row).transpose()], ignore_index=True)
+            
+            # Update current country for licences
+            current_country = row['Country']
+        elif not pd.isna(explanation) and 'arms export licences' in explanation.lower():
+            # This row belongs to licences
+            if current_country:
+                # Add country name to the licences row
+                row['Country'] = current_country
+                df_licences = pd.concat([df_licences, pd.DataFrame(row).transpose()], ignore_index=True)
+            else:
+                # No corresponding country found for licences
+                licences_to_append.append(row)
+
+    # Append any remaining licence rows
+    df_licences = pd.concat([df_licences, pd.DataFrame(licences_to_append)], ignore_index=True)
+
+    return df_exports, df_licences
+
+# Split and clean the Local Currency DataFrame
+local_currency_exports_df, local_currency_licences_df = split_and_clean(local_currency_df)
+
+# Split and clean the Current USD DataFrame
+current_usd_exports_df, current_usd_licences_df = split_and_clean(current_usd_df)
+
+# Split and clean the Constant USD DataFrame
+constant_usd_exports_df, constant_usd_licences_df = split_and_clean(constant_usd_df)
+
+# List of all dataframes
+all_dataframes = [
+    local_currency_exports_df, local_currency_licences_df,
+    current_usd_exports_df, current_usd_licences_df,
+    constant_usd_exports_df, constant_usd_licences_df
+]
+
+# Remove rows with "Country" as "nan" from all dataframes
+for df in all_dataframes:
+    df.dropna(subset=['Country'], inplace=True)
+    df.reset_index(drop=True, inplace=True)
 
 # Handle missing values based on your analysis needs (e.g., imputation or removal)
 # Consider why data is missing. If it's missing completely at random (MCAR), meaning the probability of data being missing is unrelated to the values themselves, dropping rows may be a reasonable option. However, if the missing data is not MCAR (e.g., missing systematically or based on certain conditions), dropping rows might lead to biased results.
@@ -107,48 +152,87 @@ constant_usd_df = constant_usd_df.replace('. .', pd.NA)
 # For example, if you have numerical columns, you can convert them to float or int:
 # local_currency_df['Column_Name'] = local_currency_df['Column_Name'].astype(float)
 
-# Check the structure of the cleaned DataFrames
-print("Cleaned Local Currency DataFrame:")
-print(local_currency_df.head())
+# List of names for the corresponding dataframes
+all_dataframe_names = [
+    "Local Currency Exports", "Local Currency Licences",
+    "Current USD Exports", "Current USD Licences",
+    "Constant USD Exports", "Constant USD Licences"
+]
 
-print("Cleaned Current USD DataFrame:")
-print(current_usd_df.head())
+# Define exchange rates for Dutch Guilders to Euros for each year
+exchange_rates = {
+    1994: 0.45378,
+    1995: 0.45378,
+    1996: 0.45378,
+    1997: 0.45378,
+    1998: 0.45378,
+    1999: 0.45378,
+    2000: 0.45378,
+    2001: 0.45378,
+    2002: 0.45378,
+    2003: 0.45378,
+    2004: 0.45378,
+    2005: 0.45378,
+    2006: 0.45378,
+    2007: 0.45378,
+    2008: 0.45378,
+    2009: 0.45378,
+    2010: 0.45378,
+    2011: 0.45378,
+    2012: 0.45378,
+    2013: 0.45378,
+    2014: 0.45378,
+    2015: 0.45378,
+    2016: 0.45378,
+    2017: 0.45378,
+    2018: 0.45378,
+    2019: 0.45378,
+}
 
-print("Cleaned Constant USD DataFrame:")
-print(constant_usd_df.head())
+# Iterate through the dataframe and convert Dutch Guilders to Euros
+for year in range(1994, 2020):
+    # Check if the year exists in the exchange_rates dictionary
+    if year in exchange_rates:
+        # Convert the values in Dutch Guilders to Euros
+        local_currency_licences_df[year] = local_currency_licences_df.apply(
+            lambda row: row[year] * exchange_rates[year] if row['Currency'] == 'D. Guilders (m.)' else row[year],
+            axis=1
+        )
 
-# Check for missing values and data types in the cleaned Local Currency DataFrame
-print("\nMissing Values in Cleaned Local Currency DataFrame:")
-print(local_currency_df.isnull().sum())
+# Remove rows with 'D. Guilders (m.)' in the 'Currency' column
+local_currency_licences_df = local_currency_licences_df[local_currency_licences_df['Currency'] != 'D. Guilders (m.)']
 
-print("\nData Types and Info for Cleaned Local Currency DataFrame:")
-print(local_currency_df.info())
+# Iterate through dataframes and print summary information for each
+for dataframe, name in zip(all_dataframes, all_dataframe_names):
+    print(f"Summary for {name}:\n")
 
-# Check basic statistics for the cleaned Local Currency DataFrame
-print("\nBasic Statistics for Cleaned Local Currency DataFrame:")
-print(local_currency_df.describe())
+    # Print dataframe description
+    print("Dataframe Description:")
+    print(dataframe.describe())
+    print("\n")
+    
+# Assuming your DataFrame is named 'local_currency_licences_df'
+duplicate_rows = local_currency_licences_df[local_currency_licences_df.duplicated()]
+print("Duplicate Rows:")
+print(duplicate_rows)
 
-# Check for missing values and data types in the cleaned Current USD DataFrame
-print("\nMissing Values in Cleaned Current USD DataFrame:")
-print(current_usd_df.isnull().sum())
+local_currency_licences_df = local_currency_licences_df.drop_duplicates()
+    
+# Separate loop to print first few rows for each dataframe
+for dataframe, name in zip(all_dataframes, all_dataframe_names):
+    print(f"First Few Rows for {name}:\n")
+    print(dataframe.head())
+    print("\n")
 
-print("\nData Types and Info for Cleaned Current USD DataFrame:")
-print(current_usd_df.info())
+# Separate loop to check for missing values and data types for each dataframe
+for dataframe, name in zip(all_dataframes, all_dataframe_names):
+    print(f"Missing Values for {name}:\n")
+    print(dataframe.isnull().sum())
+    print("\n")
 
-# Check basic statistics for the cleaned Current USD DataFrame
-print("\nBasic Statistics for Cleaned Current USD DataFrame:")
-print(current_usd_df.describe())
-
-# Check for missing values and data types in the cleaned Constant USD DataFrame
-print("\nMissing Values in Cleaned Constant USD DataFrame:")
-print(constant_usd_df.isnull().sum())
-
-print("\nData Types and Info for Cleaned Constant USD DataFrame:")
-print(constant_usd_df.info())
-
-# Check basic statistics for the cleaned Constant USD DataFrame
-print("\nBasic Statistics for Cleaned Constant USD DataFrame:")
-print(constant_usd_df.describe())
+    print(f"Dataframe Info for {name}:\n")
+    print(dataframe.info())
+    print("\n")
 
 # Rename columns
 new_column_names = ['Country', 'Currency'] + [f'Year_{year}' for year in range(1994, 2020)] + ['Explanation of data', 'Comments', 'Sources of data']
